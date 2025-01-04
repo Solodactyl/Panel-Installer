4ffd75552e018836a60545ef267de3f5241b93a2
SoloDactyl <flayerhost@gmail.com>
2025-01-04 10:55:57 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,6 @@ echo "2 - Uninstall SoloDactyl Panel "
 read -p "Enter your choice: " choice
 
 if [[ $choice -eq 1 ]]; then
-
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_20.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
@@ -21,7 +20,6 @@ if [[ $choice -eq 1 ]]; then
     npm run createUser
     node .
 
-    echo "Installation completed! SoloDactyl Panel is running."
 
 
 
